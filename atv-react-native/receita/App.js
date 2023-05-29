445ffc348595d7bdb6853b3c86f6a2e04f922ca8
igor <diegoigor2001@gmail.com>
2023-05-28 22:28:03 -0300

    criando rotas

--- atv-react-native/receita/App.js
+++ atv-react-native/receita/App.js
@@ -3,26 +3,49 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ReceitaList from './src/view/ReceitaList';
 import ReceitaForm from './src/view/ReceitaForm';
- 
+import { Button, Icon } from 'react-native-elements';
+
 const Stack = createStackNavigator();
 
 export default props => {
-    return(
+    return (
         <NavigationContainer>
-            <Stack.Navigator
-                initialRouterName="ReceitaList">
-
+            <Stack.Navigator initialRouterName="ReceitaList" screenOptions={screenOptions}>
                 <Stack.Screen
                     name="ReceitaList"
-                    component={ReceitaList}
+                    component={props => <ReceitaList {...props} />}
+                    options={({ navigation }) => {
+                        return {
+                            title: "Lista de Receitas",
+                            headerRight: () => (
+                                <Button
+                                    onPress={() => navigation.navigate('ReceitaForm')}
+                                    type='clear'
+                                    icon={<Icon name="add" size={35} color='white' />}
+                                />
+                            )
+                        }
+                    }}
                 />
 
                 <Stack.Screen
                     name="ReceitaForm"
                     component={ReceitaForm}
+                    options={{
+                        title: "Formulário de Receitas"
+                    }}
                 />
-
             </Stack.Navigator>
         </NavigationContainer>
-    )
+    );
+}
+
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: '#f4511e'
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+        fontWeight: 'bold'
+    }
 }
--- atv-react-native/receita/src/view/ReceitaList.js
+++ atv-react-native/receita/src/view/ReceitaList.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import receitas from '../view/data/Receitas'
 
 export default props => {
+
+    function getReceitaItem({item}){
+        return (
+            <ListItem
+            leftAvatar={{ source: { uri: item.img } }}
+            key={item.id}
+            title={item.titulo}
+            bottomDivider
+       
+            />
+            )
+    }
+
     return(
-        <Text>Lista de Receitas</Text>
+        <View>
+            <FlatList
+                keyExtractor={ receita => receita.id.toString()}
+                data={receitas}
+                renderItem={getReceitaItem}
+            />
+        </View>
     )
 }

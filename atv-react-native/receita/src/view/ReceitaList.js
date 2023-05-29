77a0a73cfe6a828119b9756321f8a8b25cbc8397
igor <diegoigor2001@gmail.com>
2023-05-29 20:55:02 -0300

    trabalhando na listagem das receitas

--- atv-react-native/receita/src/view/ReceitaList.js
+++ atv-react-native/receita/src/view/ReceitaList.js
@@ -1,29 +1,44 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
-import receitas from '../view/data/Receitas'
+import React from "react";
+import { View, Text } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import receitas from "../view/data/Receitas";
+import { ListItem, Avatar } from "react-native-elements";
+import { Button, Icon } from 'react-native-elements';
 
-export default props => {
 
-    function getReceitaItem({item}){
-        return (
-            <ListItem
-            leftAvatar={{ source: { uri: item.img } }}
-            key={item.id}
-            title={item.titulo}
-            bottomDivider
-       
-            />
-            )
-    }
+export default (props) => {
+  function getActions(receita) {
+    console.log('tá funcionando va descansar workarround na veia!!')
+    props.navigation.navigate("ReceitaForm", receita)
+  }
 
-    return(
-        <View>
-            <FlatList
-                keyExtractor={ receita => receita.id.toString()}
-                data={receitas}
-                renderItem={getReceitaItem}
-            />
-        </View>
-    )
-}
+  function getReceitaItem({ item: receita }) {
+    return (
+      <ListItem
+        key={receita.id}
+        bottomDivider
+        onPress={() => props.navigation.navigate("ReceitaForm", receita)}
+      >
+        <Avatar source={{ uri: receita.img }} />
+        <ListItem.Content>
+          <ListItem.Title>{receita.titulo}</ListItem.Title>
+        </ListItem.Content>
+        <Button
+        onPress={() => getActions(receita)}
+        type="clear"
+        icon={<Icon name="edit" size={35} color="orange" />}
+      />
+      </ListItem>
+    );
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <FlatList
+        keyExtractor={(receita) => receita.id.toString()}
+        data={receitas}
+        renderItem={getReceitaItem}
+      />
+    </View>
+  );
+};
